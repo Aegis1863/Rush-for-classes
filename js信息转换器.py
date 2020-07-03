882f965432418765474e0669b2e419804046469c
Lee SunBowen <1554849150@qq.com>
2020-07-03 10:46:02 +0800

    Update js信息转换器.py
    这是一个js信息转化器，在网页上复制到headers和data信息之后，运行此程序，按照提示粘贴上信息，运行后会给出python字典的信息，再将字典内容写入抢课程序中即可

--- js信息转换器.py
+++ js信息转换器.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 21 01:15:41 2020
-
-@author: LiSunBowen
-"""
-
 def tran(t):
     if t!='':
         p=dict([line.split(": ",1) for line in t.split("\n")])
