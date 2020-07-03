a717013630654e312910f8f72072006f27c678fb
Lee SunBowen <1554849150@qq.com>
2020-07-03 09:23:38 +0800

    增加了效果图片和readme解释

--- 抢课测试.py
+++ 抢课测试.py
@@ -8,15 +8,16 @@ Created on Sat Jun 20 23:42:23 2020
 import requests
 import time
 
-url='http://218.197.80.13/jwglxt/xsxk/zzxkyzb_xkBcZyZzxkYzb.html?gnmkdm=N253512&su=18020115'
+urlh=input('请输入url：')
+url=urlh
 header={'Accept': 'application/json, text/javascript, */*; q=0.01', 'Accept-Encoding': 'gzip, deflate', 'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6', 'Connection': 'keep-alive', 'Content-Length': '659', 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8', 'Cookie': 'JSESSIONID=EB7FC341DC5217219226FDEE5ED8BD36', 'DNT': '1', 'Host': '218.197.80.13', 'Origin': 'http://218.197.80.13', 'Referer': 'http://218.197.80.13/jwglxt/xsxk/zzxkyzb_cxZzxkYzbIndex.html?gnmkdm=N253512&layout=default&su=18020115', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.56', 'X-Requested-With': 'XMLHttpRequest'}
 
-#header=dict([line.split(": ",1) for line in header[i].split("\n")])
+#header=dict([line.split(": ",1) for line in header[i].split("\n")]) 已弃用
 data=[
 {'jxb_ids': '75105f21f5f71f114f0e0d582f0b93363f0f6e85928de0f68d701e4f806aa85a2e3fe2c0838b8980461af5907fa5e87b795e22d7f31d80ced5b6fe14befc045dcdcf5752fbb163c91ea4fee4b3d930b889e833036c226e3f43344505e631b9ecdefcddc54c6d5bac5b0b8cca4e372d4c8ee733bb641b5b4c4fccb4659b601ad8', 'kch_id': '7D33DD0A6668723CE0530B50C5DA0DBB', 'kcmc': '(TX3431)当代中国政府与政治(网络通识课) - 2.0 学分', 'rwlx': '2', 'rlkz': '0', 'rlzlkz': '1', 'sxbj': '1', 'xxkbj': '0', 'qz': '0', 'cxbj': '0', 'xkkz_id': 'A83E41F4B6362D30E0530B50C5DA0F14', 'njdm_id': '2018', 'zyh_id': '0307', 'kklxdm': '10', 'xklc': '2', 'xkxnm': '2020', 'xkxqm': '3'},
 {'jxb_ids': '37bc628e36f5670d096b76d137588ba836ef1623fd275fae2fd9b07ce33cb9b01d390173d7ee30aa66a0689da389cb8aafd4793c5a5299ac56c273f0c63208a73c982e5e4ef31c3a5cf1804f07a9b9647b3c1a32aed983dc4e24acdb8f989bfc960cda1809cc2afcaf48d79fe5bcbf3247366f84453b4cb4f4e6dde3a13357d8', 'kch_id': '8C3555B8B84533A1E0530B50C5DA571C', 'kcmc': '(TX9845)聆听心声：音乐审美心理分析(网络通识课) - 2.0 学分', 'rwlx': '2', 'rlkz': '0', 'rlzlkz': '1', 'sxbj': '1', 'xxkbj': '0', 'qz': '0', 'cxbj': '0', 'xkkz_id': 'A83E41F4B6362D30E0530B50C5DA0F14', 'njdm_id': '2018', 'zyh_id': '0307', 'kklxdm': '10', 'xklc': '2', 'xkxnm': '2020', 'xkxqm': '3'},
 ]
-#data[i]=dict([line.split(": ",1) for line in data.split("\n")])
+#data[i]=dict([line.split(": ",1) for line in data.split("\n")]) 已弃用
 
 t=-1 #初始化t值，t是抢课后服务器返回值，返回-1表示因为满员或其他原因未抢课成功，返回1表示抢课成功
 num=0 #计数器，记录重复发送了多少次
